9b79ec422233fabe4a07b28499417b34f2d429a7
Maher Deeb <maherdeebcv@gmail.com>
2018-10-03 22:03:44 +0200

    The first version of ETL is ready. comments are missing

--- ETL.py
+++ ETL.py
@@ -2,4 +2,135 @@
 """
 @author: Maher Deeb
 """
+import os
+import pandas as pd
+from keras.preprocessing.image import ImageDataGenerator,\
+    array_to_img, img_to_array, load_img
+from skimage.transform import resize
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-white')
+
+
+path_train = './data/images/'
+path_train_mask = './data/masks/'
+path_test = './data/images_test/'
+path_depth = './data/depths.csv'
+
+
+
+def _images_id(path):
+    
+    _ids = next(os.walk(path))[2]
+    
+    return _ids
+
+def _load_depth(path):
+    
+    df = pd.read_csv(path)
+    
+    return df
+
+def _load_an_image(path,_id):
+    
+    loaded_image = load_img(path + _id)
+    
+    return loaded_image
+
+def _img_to_array(loaded_image):
+    
+    _array = img_to_array(loaded_image)[:,:,1]
+    
+    return _array
+
+def _resize_image_size(train_x,new_size):
+    
+    new_image = resize(train_x, (new_size, new_size, 1), mode='constant',
+                       preserve_range=True)
+    
+    return new_image
+
+def plot_sample_image(train_x,train_x_resized,train_y,train_y_resized,dataset):
+    
+    plt.figure(1)
+    plt.imshow(train_x)
+    plt.show()
+    plt.figure(2)
+    plt.imshow(train_x_resized[:,:,0])
+    plt.show()
+    if dataset is "train":
+        plt.figure(3)
+        plt.imshow(train_y)
+        plt.show()
+        plt.figure(4)
+        plt.imshow(train_y_resized[:,:,0])
+        plt.show()
+    
+def ETL_data_loading(new_size,plot_train_sample = False,
+                     plot_test_sample = False):
+
+    train_ids = _images_id(path_train)
+    train_mask_ids = _images_id(path_train_mask)
+    test_ids = _images_id(path_test)
+    dataframe_depth = _load_depth(path_depth)
+
+    print("There are {} images in the train dataset".format(len(train_ids)))
+    print("There are {} images in the test dataset".format(len(test_ids)))
+    print("The depth dataset contains {} data points".format(
+            len(dataframe_depth)))
+    print("The first 5 rows of the depth dataframe:")
+    print(dataframe_depth.head())
+    
+    sample_id = 110
+
+    # prepare train data
+    train_x_resized = np.zeros((len(train_ids), new_size,
+                                new_size, 1), dtype=np.uint8)
+    train_y_resized = np.zeros((len(train_ids),
+                                new_size, new_size, 1), dtype=np.bool)
+    count_image = 0
+    
+    for train_id_i in train_ids:
+        
+        loaded_train_image = _load_an_image(path_train,train_id_i)
+        train_x = _img_to_array(loaded_train_image)
+        train_x_resized[count_image] = _resize_image_size(train_x,new_size)
+        
+        loaded_train_mask = _load_an_image(path_train_mask,train_id_i)
+        train_y = _img_to_array(loaded_train_mask)
+        train_y_resized[count_image] =  _resize_image_size(train_y,new_size)
+        
+        if count_image == sample_id and plot_train_sample:
+            
+            plot_sample_image(train_x,
+                            train_x_resized[count_image],
+                            train_y,
+                            train_y_resized[count_image],
+                            "train")
+        count_image +=1
+
+    # prepare test data
+    sample_id_test = 100
+    test_x_resized = np.zeros((len(test_ids), new_size,
+                                new_size, 1), dtype=np.uint8)
+    count_image = 0
+    
+    for test_id_i in test_ids:
+        
+        loaded_test_image = _load_an_image(path_test,test_id_i)
+        test_x = _img_to_array(loaded_test_image)
+        test_x_resized[count_image] = _resize_image_size(test_x,new_size)
+        if count_image == sample_id and sample_id_test:
+            plot_sample_image(test_x,
+                            test_x_resized[count_image],
+                            0,
+                            0,
+                            "test")
+        count_image +=1
+    
+    return dataframe_depth, train_x_resized, train_y_resized, test_x_resized
+
+#ETL_data_loading(128,True,False)
+
+    
 
