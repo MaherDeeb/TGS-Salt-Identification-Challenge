7743358f151e6009ecf2ccf982450d5340741614
Maher Deeb <maherdeebcv@gmail.com>
2018-10-08 23:45:52 +0200

    add warp padding

--- padding.py
+++ padding.py
@@ -10,7 +10,8 @@ def _padding_image_constant_0(array_to_pad):
     # padded_array =np.pad(array_to_pad, ((14,13),(14,13),(0,0)), 'constant',
     #                      constant_values=(0, 0))
     
-    padded_array =np.pad(array_to_pad, ((14,13),(14,13),(0,0)), 'symmetric')
+    #padded_array =np.pad(array_to_pad, ((14,13),(14,13),(0,0)), 'symmetric')
+    padded_array =np.pad(array_to_pad, ((14,13),(14,13),(0,0)), 'wrap')
     return padded_array
 
 
